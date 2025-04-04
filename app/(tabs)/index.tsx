3eaca4da3caa90b6056707b7341bea709445ca8d
atharsh07 <atharsh0425@gmail.com>
2025-04-04 19:19:41 +0530

    auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="justify-center items-center flex-1">
-        <Text className="text-5xl text-blue-500 font-bold">Welcome!</Text>
+        <Text className="text-5xl text-accent font-bold">Welcome!</Text>
     </View>
   );
 }
+
